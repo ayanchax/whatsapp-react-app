87ee9b35cf529188678eb2212c45da9dd1c67df8
Swaroop <swaroop.chakraborty@cerner.com>
2021-06-11 14:44:48 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
+import Sidebar from "./Sidebar";
+
 function App() {
-  return <div className="App" >
-    Whatsapp clone
-  </div>;
+  return (
+    // BEM convention
+    <div className="app" >
+      <div className="app__body">
+
+        <Sidebar />
+
+        {/* Chat */}
+      </div>
+
+    </div>
+  );
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDk6yUFd8xrtmvPtRJK_l9Dy_KHwvJpo7o",
+    authDomain: "whatsapp-clone-1590c.firebaseapp.com",
+    projectId: "whatsapp-clone-1590c",
+    storageBucket: "whatsapp-clone-1590c.appspot.com",
+    messagingSenderId: "998631441263",
+    appId: "1:998631441263:web:620b759813f1c52a389e9a",
+    measurementId: "G-MQKRX4NVMS",
+};
